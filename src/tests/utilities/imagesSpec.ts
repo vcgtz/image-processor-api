f11423183ebc0cab1f3ea6b292d4329e720eebd7
Vicente Gutiérrez <gtz.rvera@gmail.com>
2021-11-04 22:44:23 -0600

    🔬 Add tests to check the creation of the resized images

--- src/tests/utilities/imagesSpec.ts
+++ src/tests/utilities/imagesSpec.ts
@@ -1,15 +1,46 @@
-import { existFile, existFolder } from '../../utilities/images';
+import sharp from 'sharp';
+import fsPromises from 'fs/promises';
+import * as images from '../../utilities/images';
 
 describe('Testing utilities to work with files', () => {
-  it('resolve existsFile when the file exists', async () => {
-    await expectAsync(existFile('santamonica')).toBeResolved();
+  it('resolve existsImage when the file exists', async () => {
+    await expectAsync(images.existImage('santamonica.jpg')).toBeResolvedTo(true);
   });
 
   it('reject existsFile when the file exists', async () => {
-    await expectAsync(existFile('unknown')).toBeResolvedTo(false);
+    await expectAsync(images.existImage('unknown')).toBeResolvedTo(false);
   });
 
   it('resolve existFolder when the folder exists', async () => {
-    await expectAsync(existFolder('images')).toBeResolved();
+    await expectAsync(images.existFolder('images')).toBeResolvedTo(true);
+  });
+});
+
+describe('Testing creation of thumbnails', () => {
+  beforeEach(async () => {
+    const existsThumbFolder = await images.existFolder(images.THUMBS_FOLDER);
+
+    if (!existsThumbFolder) {
+      await images.createFolder(images.THUMBS_FOLDER);
+    }
+  });
+
+  it('creates 50x50 image', async () => {
+    const imagePath = `${images.IMAGES_FOLDER}/santamonica.jpg`;
+    const thumbFilename = '50_50_testing.jpg';
+    const thumbImagePath = `${images.THUMBS_FOLDER}/${thumbFilename}`;
+
+    await sharp(imagePath)
+      .resize(50, 50)
+      .toFile(thumbImagePath);
+    
+    await expectAsync(images.existThumbImage(thumbFilename)).toBeResolvedTo(true);
+  });
+
+  afterAll(async () => {
+    const thumbFilename = '50_50_testing.jpg';
+    const thumbImagePath = `${images.THUMBS_FOLDER}/${thumbFilename}`;
+
+    await fsPromises.unlink(thumbImagePath);
   });
 });
